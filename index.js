180507b4a4ced95b9c901ba2f9fe8bc483311b31
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-09 23:03:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ fetch("http://localhost:3000/users", configObj)
   .then(function(response) {
     return response.json();
   })
-  .then(function(object))
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! ")
+  })
 }
 
 
